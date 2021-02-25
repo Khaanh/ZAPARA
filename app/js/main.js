40a50ed9a411b24c66cb59cd7e13edfb15bbb3ac
Nguyen Ha Khanh <nguyen.hakhanh@avanquest.com>
2021-02-25 14:50:57 +0200

    Animate

--- app/js/main.js
+++ app/js/main.js
@@ -2,3 +2,16 @@ $(function () {
 	//= include/default
 	//= include/sliders
 });
+
+let animate = document.querySelector('#js-textAnimate');
+let target;
+
+animate.onmouseenter = function (e) {
+	target = e;
+	target.toElement.children[0].classList.add('is-scale')
+}
+
+animate.onmouseleave = function (e) {
+	target.toElement.children[0].classList.remove('is-scale')
+}
+
